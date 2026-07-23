a6b9f50a1cf52500d033f69185a556b0b0c78e3c
César Roberto <cesarobertovieirarocha@gmail.com>
2026-07-23 12:22:02 -0300

    Add files via upload

---
+++ data/players.js
@@ -0,0 +1,36 @@
+/* ========================================
+   DADOS DOS JOGADORES (ESTATÍSTICAS)
+   Consumido por: js/player.js
+======================================== */
+var playerStatsData = {
+    "Alvaro Freitas": { jogos: 0, gols: 0, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 0, empates: 0, derrotas: 0 },
+    "André Luiz": { jogos: 4, gols: 16, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 4, empates: 0, derrotas: 0 },
+    "Bruno Teixeira": { jogos: 3, gols: 1, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 3, empates: 0, derrotas: 0 },
+    "César Roberto": { jogos: 5, gols: 3, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 5, empates: 0, derrotas: 0 },
+    "César Rodrigues": { jogos: 2, gols: 1, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 2, empates: 0, derrotas: 0 },
+    "Claudio Junior": { jogos: 0, gols: 0, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 0, empates: 0, derrotas: 0 },
+    "Douglas Lima": { jogos: 1, gols: 1, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 0, empates: 0, derrotas: 0 },
+    "Eduardo Frange": { jogos: 4, gols: 6, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 4, empates: 0, derrotas: 0 },
+    "Gabriel Augusto": { jogos: 2, gols: 14, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 2, empates: 0, derrotas: 0 },
+    "Gabriel Fagundes": { jogos: 1, gols: 0, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 1, empates: 0, derrotas: 0 },
+    "Ian Gois": { jogos: 4, gols: 3, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 4, empates: 0, derrotas: 0 },
+    "Jan Fontes": { jogos: 4, gols: 2, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 4, empates: 0, derrotas: 0 },
+    "Jhonny Soares": { jogos: 2, gols: 2, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 2, empates: 0, derrotas: 0 },
+    "João Renato": { jogos: 1, gols: 4, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 1, empates: 0, derrotas: 0 },
+    "João Vitor": { jogos: 0, gols: 0, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 0, empates: 0, derrotas: 0 },
+    "Leonardo Quirino": { jogos: 4, gols: 2, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 4, empates: 0, derrotas: 0 },
+    "Lucas Malveira": { jogos: 2, gols: 11, assistencias: 0, cartoesAmarelos: 1, cartoesVermelhos: 0, vitorias: 2, empates: 0, derrotas: 0 },
+    "Lucas Reimer": { jogos: 5, gols: 33, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 5, empates: 0, derrotas: 0 },
+    "Marco Carmin": { jogos: 0, gols: 0, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 0, empates: 0, derrotas: 0 },
+    "Matheus Duarte": { jogos: 3, gols: 0, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 3, empates: 0, derrotas: 0 },
+    "Natan Calixto": { jogos: 2, gols: 0, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 2, empates: 0, derrotas: 0 },
+    "Nillander Bonfim": { jogos: 4, gols: 16, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 4, empates: 0, derrotas: 0 },
+    "Paulo Linhares": { jogos: 3, gols: 3, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 3, empates: 0, derrotas: 0 },
+    "Pedro Amaral": { jogos: 3, gols: 6, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 3, empates: 0, derrotas: 0 },
+    "Pedro Lucas": { jogos: 1, gols: 6, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 1, empates: 0, derrotas: 0 },
+    "Pedro Henrique": { jogos: 2, gols: 3, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 2, empates: 0, derrotas: 0 },
+    "Raphael Gouveia": { jogos: 0, gols: 0, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 0, empates: 0, derrotas: 0 },
+    "Suelso Junior": { jogos: 1, gols: 8, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 1, empates: 0, derrotas: 0 },
+    "Thiago Tolomelli": { jogos: 3, gols: 2, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 3, empates: 0, derrotas: 0 },
+    "Tulio Gabriel": { jogos: 0, gols: 0, assistencias: 0, cartoesAmarelos: 0, cartoesVermelhos: 0, vitorias: 0, empates: 0, derrotas: 0 }
+};
